0f02def6e2005c04b20bfd04f68f8b9044966d04
thousandemon <william@fairfieldco.com>
2023-10-22 18:37:58 -0400

    Refactoring

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -24,44 +24,53 @@ public class Customer {
 
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
+        int TotalFrequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
+
         for (Rental each : rentals) {
-            double thisAmount = 0;
+            result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(getThisAmount(each)) + "\n";
 
-            //determine amounts for each line
-            switch (each.getCar().getPriceCode()) {
-                case Car.STANDARD:
-                    thisAmount += 200;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented()-2)*150;
-                    }
-                    break;
-
-                case Car.LUXURY:
-                    thisAmount += each.getDaysRented() * 500;
-                    break;
-
-                case Car.COMPACT:
-                    thisAmount += 150;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented()-3)*150;
-                    }
-                    break;
-            }
-
-            frequentRenterPoints++;
-            if ((each.getCar().getPriceCode() == Car.LUXURY) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-
+            totalAmount += getThisAmount(each);
+            TotalFrequentRenterPoints += getFrequentRenterPointsFor(each);
         }
+
     result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    result += "You earned " + String.valueOf(TotalFrequentRenterPoints) + " frequent renter points";
     return result;
 
+    }
+
+    private static int getFrequentRenterPointsFor(Rental rental) {
+        int thisFrequentRenterPoints = 1;
+
+        if ((rental.getCar().getPriceCode() == Car.LUXURY) && rental.getDaysRented() > 1) {
+            thisFrequentRenterPoints++;
+        }
+        return thisFrequentRenterPoints;
+    }
+
+    private static double getThisAmount(Rental rental) {
+        double thisAmount = 0;
+
+        switch (rental.getCar().getPriceCode()) {
+            case Car.STANDARD:
+                thisAmount += 200;
+                if (rental.getDaysRented() > 2) {
+                    thisAmount += (rental.getDaysRented()-2)*150;
+                }
+                break;
+
+            case Car.LUXURY:
+                thisAmount += rental.getDaysRented() * 500;
+                break;
+
+            case Car.COMPACT:
+                thisAmount += 150;
+                if (rental.getDaysRented() > 3) {
+                    thisAmount += (rental.getDaysRented()-3)*150;
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
