44b1590df86f75a18c3397c28d78539cddac310c
thousandemon <william@fairfieldco.com>
2023-10-23 18:25:08 -0400

    Moved Methods to Rental Class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -28,10 +28,10 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each : rentals) {
-            result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(getThisAmount(each)) + "\n";
+            result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(each.getCharge()) + "\n";
 
-            totalAmount += getThisAmount(each);
-            TotalFrequentRenterPoints += getFrequentRenterPointsFor(each);
+            totalAmount += each.getCharge();
+            TotalFrequentRenterPoints += each.getFrequentRenterPoints();
         }
 
     result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
@@ -40,37 +40,4 @@ public class Customer {
 
     }
 
-    private static int getFrequentRenterPointsFor(Rental rental) {
-        int thisFrequentRenterPoints = 1;
-
-        if ((rental.getCar().getPriceCode() == Car.LUXURY) && rental.getDaysRented() > 1) {
-            thisFrequentRenterPoints++;
-        }
-        return thisFrequentRenterPoints;
-    }
-
-    private static double getThisAmount(Rental rental) {
-        double thisAmount = 0;
-
-        switch (rental.getCar().getPriceCode()) {
-            case Car.STANDARD:
-                thisAmount += 200;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented()-2)*150;
-                }
-                break;
-
-            case Car.LUXURY:
-                thisAmount += rental.getDaysRented() * 500;
-                break;
-
-            case Car.COMPACT:
-                thisAmount += 150;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented()-3)*150;
-                }
-                break;
-        }
-        return thisAmount;
-    }
 }
--- src/main/java/org/example/Rental.java
+++ src/main/java/org/example/Rental.java
@@ -9,6 +9,39 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    int getFrequentRenterPoints() {
+        if ((getCar().getPriceCode() == Car.LUXURY) && getDaysRented() > 1) {
+            return 2;
+        }else{
+            return 1;
+        }
+    }
+
+    double getCharge() {
+        double result = 0;
+
+        switch (getCar().getPriceCode()) {
+            case Car.STANDARD:
+                result += 200;
+                if (getDaysRented() > 2) {
+                    result += (getDaysRented()-2)*150;
+                }
+                break;
+
+            case Car.LUXURY:
+                result += getDaysRented() * 500;
+                break;
+
+            case Car.COMPACT:
+                result += 150;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented()-3)*150;
+                }
+                break;
+        }
+        return result;
+    }
+
     public Car getCar() {
         return car;
     }
