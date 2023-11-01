be5c1f590a8c876a280907d3c4cfba8d19250be5
BackpackTrainer <bill@fairfieldco.com>
2023-11-01 12:23:20 -0400

    added variable for frequent renter points

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -7,6 +7,7 @@ public class Customer {
 
     private String name;
     private List<Rental> rentals;
+    int frequentRentalPoints = 0;
 
     public Customer(String name) {
         this.name = name;
@@ -21,35 +22,34 @@ public class Customer {
     }
 
 
-
     public String statement() {
         double totalAmount = 0;
-        int TotalFrequentRenterPoints = 0;
+        int totalFrequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each : rentals) {
             result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(getThisAmount(each)) + "\n";
 
             totalAmount += getThisAmount(each);
-            TotalFrequentRenterPoints += getFrequentRenterPointsFor(each);
+            totalFrequentRenterPoints += getFrequentRenterPointsFor(each);
         }
-
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(TotalFrequentRenterPoints) + " frequent renter points";
-    return result;
-
+        frequentRentalPoints += totalFrequentRenterPoints;
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(totalFrequentRenterPoints) + " frequent renter points";
+        return result;
     }
 
-    private static int getFrequentRenterPointsFor(Rental rental) {
+    private int getFrequentRenterPointsFor(Rental rental) {
         int thisFrequentRenterPoints = 1;
 
         if ((rental.getCar().getPriceCode() == Car.LUXURY) && rental.getDaysRented() > 1) {
             thisFrequentRenterPoints++;
         }
+
         return thisFrequentRenterPoints;
     }
 
-    private static double getThisAmount(Rental rental) {
+    private  double getThisAmount(Rental rental) {
         double thisAmount = 0;
 
         switch (rental.getCar().getPriceCode()) {
