be0df5571ff4d1794b9589db2d8c511984305d68
BackpackTrainer <bill@fairfieldco.com>
2023-11-01 13:30:33 -0400

    added variable for the frequent rental points

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -7,6 +7,7 @@ public class Customer {
 
     private String name;
     private List<Rental> rentals;
+    int frequentRentalPoints = 0;
 
     public Customer(String name) {
         this.name = name;
@@ -20,22 +21,18 @@ public class Customer {
         rentals.add(arg);
     }
 
-
-
     public String statement() {
         String result = "Rental Record for " + getName() + "\n";
 
-
         for (Rental each : rentals) {
             result += "\t" + each.getCar().getMakeModel() + "\t" + String.valueOf(each.getCharge()) + "\n";
-
         }
-
+        frequentRentalPoints += getTotalFrequentRenterPoints();
 
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-    result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-    return result;
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 
+    return result;
     }
     private double getTotalCharge() {
         double totalCharge = 0;
@@ -45,11 +42,10 @@ public class Customer {
         return totalCharge;
     }
     private int getTotalFrequentRenterPoints() {
-        int TotalFrequentRenterPoints = 0;
+        int totalFrequentRenterPoints = 0;
         for (Rental each : rentals) {
-            TotalFrequentRenterPoints += each.getFrequentRenterPoints();
+            totalFrequentRenterPoints += each.getFrequentRenterPoints();
         }
-        return TotalFrequentRenterPoints;
+        return totalFrequentRenterPoints;
     }
-    
 }
